774bb8bb22d07cb7917f8cc58707ab695d5d1b7b
Deka Louis <dekarebaudo@gmail.com>
2026-08-15 14:39:17 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::env;
+use std::path::Path;
+use std::os::unix::fs::PermissionsExt;
 
 fn main() {
     loop {
@@ -12,6 +15,8 @@ fn main() {
             .unwrap();
         
         let command = command.trim().to_string();
+        // path var 
+        let path_var = env::var("PATH").unwrap_or_default();
 
         if command == "exit" {
             break;
@@ -24,8 +29,19 @@ fn main() {
             let arg = &command[5..];
             if matches!(arg, "echo" | "exit" | "type") {
                 println!("{arg} is a shell builtin");
-            } else if let Ok(path) = which::which(&command[5..]) {
-                println!("{} is {}", &command[5..], path.display());
+            } else if let Some(matched_path) = path_var
+                .split(':')
+                .map(|dir| Path::new(dir).join(arg))
+                .find(|file_path| {
+                    file_path.is_file() && file_path
+                        .metadata()
+                        .map(|meta| meta.permissions().mode() & 0o111 != 0)
+                        .unwrap_or(false)
+                })
+            {
+                println!("{arg} is {}", matched_path.display());
+            // } else if let Ok(path) = which::which(&command[5..]) {
+            //     println!("{} is {}", &command[5..], path.display());
             } else {
                 println!("{}: not found", arg);
             }
